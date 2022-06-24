8152901b9147d2ce81d3a5f6a439ce5d0af02412
Divya <101665482+idivyamv@users.noreply.github.com>
2022-06-24 19:14:25 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ app.get('/api/readbook', function(req, res) {
         })
 })
 
-/* app.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('i am divs dddd')
-}) */
+})
 app.post('/api/addbook', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
@@ -133,6 +133,9 @@ app.post('/api/addbook', (req, res) => {
     res.sendFile(path.join(__dirname + '/dist/library-frontend/index.html'));
   });
 const port = 3000;
-app.listen(3000, () => {
+/* app.listen(3000, () => {
   console.log("Server up in Port 3000 ");
+}); */
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
